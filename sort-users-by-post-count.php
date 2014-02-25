82f533d603b8a8c2f3da96a79f8798e5dabb1932
Katherine Semel <ksemel@gmail.com>
2014-02-25 12:43:07 -0500

    Removing undeclared function

--- sort-users-by-post-count.php
+++ sort-users-by-post-count.php
@@ -12,9 +12,6 @@ if ( ! class_exists('Sort_Users_By_Post_Count') ) {
         function Sort_Users_By_Post_Count() {
             // Make user table sortable by post count
             add_filter( 'manage_users_sortable_columns', array( $this, 'add_custom_user_sorts' ) );
-
-            // Store info from a user's last login as user meta
-            add_action( 'wp_login', array( $this, 'user_last_login' ), 10, 2 );
         }
 
         /* Add sorting by post count to user page */
